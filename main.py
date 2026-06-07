68aaaa93af4e3f871f36cf62f463b66d3ac2539b
onlydudu2005-png <onlydudu2005@gmail.com>
2026-06-07 02:00:04 -0300

    corrigido detector de live

--- main.py
+++ main.py
@@ -30,18 +30,18 @@ async def verificar_live():
 
             try:
                 task = asyncio.create_task(client.connect())
-                await asyncio.sleep(5)
+                await asyncio.sleep(8)
+                if not task.done():
+                    if not avisou:
+                        canal = bot.get_channel(int(CHANNEL_ID))
+                        if canal is not None:
+                            await canal.send(
+                                f"🔴 LIVE ABERTA NO TIKTOK!\n"
+                                f"https://www.tiktok.com/@{TIKTOK_USER}/live"
+                            )
+                        avisou = True
 
-                if not avisou:
-                    canal = bot.get_channel(CHANNEL_ID)
-                    if canal is not None:
-                        await canal.send(
-                            f"🔴 LIVE ABERTA NO TIKTOK!\n"
-                            f"https://www.tiktok.com/@{TIKTOK_USER}/live"
-                        )
-                    avisou = True
-
-                task.cancel()
+                    task.cancel()
             except Exception as e:
                 avisou = False
                 print(f"Não está em live ou deu erro: {e}")
